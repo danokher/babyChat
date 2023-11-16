4e8c6080c8d49393d08a31096dc849012d98f30e
danokher <danokher@gmail.com>
2023-11-16 06:54:58 +0100

    ok

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
@@ -20,7 +20,7 @@ app.post("/authenticate", async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
-    console.log("Server is running on port 3001");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
 
